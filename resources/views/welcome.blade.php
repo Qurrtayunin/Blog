dbdda5726415bbf022cd4a9fc44caa615eaa3df8
Qurrota <qurrotaayunin36@gmail.com>
2019-12-06 14:00:37 +0700

    konflik branch master

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -73,7 +73,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Arum Alra Warani <hr> Qurrota A'yunin
+                    Konflik Branch Master
                 </div>
 
                 <div class="links">
