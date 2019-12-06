d672d508883baf9189deb973abb5f27246e7c854
Qurrota <qurrotaayunin36@gmail.com>
2019-12-06 14:04:26 +0700

    konflik di branch master

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -73,7 +73,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Konflik Branch Master
+                    Konflik di Branch Master
                 </div>
 
                 <div class="links">
